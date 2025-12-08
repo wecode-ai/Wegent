3a03205b4ba215002857974eaa20df6a3af52065
axb <qindi@staff.weibo.com>
2025-12-08 18:32:33 +0800

    chore: update base image version

--- docker/base/build.sh
+++ docker/base/build.sh
@@ -3,4 +3,4 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # todo: placeholder for now, will be changed to open source project later
-docker buildx build --network=host  --platform linux/amd64,linux/arm64 -t ghcr.io/wecode-ai/wegent-base-python3.12:1.0.0 -f Dockerfile .
+docker buildx build --network=host  --platform linux/amd64,linux/arm64 -t ghcr.io/wecode-ai/wegent-base-python3.12:1.0.1 -f Dockerfile .
