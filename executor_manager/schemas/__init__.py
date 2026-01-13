b4b240a0d6bc7ecc1313520d0af96c5a68a886f1
feifei <yansheng3@staff.weibo.com>
2026-01-13 11:36:28 +0800

    feat: supports the creation of subagents via the e2b protocol. (#15)

---
+++ executor_manager/schemas/__init__.py
@@ -0,0 +1,26 @@
+# SPDX-FileCopyrightText: 2025 WeCode, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""Schemas package for executor_manager API."""
+
+from executor_manager.schemas.sandbox import (CreateSandboxRequest,
+                                              CreateSandboxResponse,
+                                              ExecuteRequest, ExecuteResponse,
+                                              ExecutionStatusResponse,
+                                              KeepAliveRequest,
+                                              KeepAliveResponse,
+                                              SandboxStatusResponse,
+                                              TerminateSandboxResponse)
+
+__all__ = [
+    "CreateSandboxRequest",
+    "CreateSandboxResponse",
+    "SandboxStatusResponse",
+    "TerminateSandboxResponse",
+    "KeepAliveRequest",
+    "KeepAliveResponse",
+    "ExecuteRequest",
+    "ExecuteResponse",
+    "ExecutionStatusResponse",
+]
