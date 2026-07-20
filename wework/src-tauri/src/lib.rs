10300ea492e3456ced843fed73d218d56479b48b
yunpeng7 <yunpeng7@staff.weibo.com>
2026-07-20 13:21:41 +0800

    refactor(wework): move VNC session into wecode

--- wework/src-tauri/src/lib.rs
+++ wework/src-tauri/src/lib.rs
@@ -4,7 +4,6 @@ mod embedded_browser;
 mod local_executor;
 mod local_terminal;
 mod process_environment;
-mod vnc_session;
 mod wecode;
 mod workbench_background;
 
@@ -3683,7 +3682,7 @@ pub fn run() {
     let app = builder
         .manage(appshots::AppshotState::default())
         .manage(embedded_browser::EmbeddedBrowserState::default())
-        .manage(vnc_session::VncSessionState::default())
+        .manage(wecode::vnc_session::VncSessionState::default())
         .manage(MainWindowLifecycleState::default())
         .manage(LocalWorkspaceOpenState::default())
         .manage(TrayVisualState::default())
@@ -3790,8 +3789,8 @@ pub fn run() {
             embedded_browser::embedded_browser_relabel,
             embedded_browser::embedded_browser_resume_download,
             embedded_browser::embedded_browser_set_bounds,
-            vnc_session::get_vnc_session_config,
-            vnc_session::prepare_vnc_session,
+            wecode::vnc_session::get_vnc_session_config,
+            wecode::vnc_session::prepare_vnc_session,
             local_terminal::close_local_terminal,
             workbench_background::import_workbench_background,
             workbench_background::remove_workbench_background,
