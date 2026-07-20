5701777536a84cb874c6d0d54fceab2ace5d3765
yunpeng7 <yunpeng7@staff.weibo.com>
2026-07-20 20:02:41 +0800

    refactor(wework): move desktop launch behavior into wecode

--- wework/wecode/extensions/cloud-desktop.tsx
+++ wework/wecode/extensions/cloud-desktop.tsx
@@ -1,11 +1,13 @@
 import type { CloudDesktopExtension } from '@/extensions/cloud-desktop-contract'
 import { VncDesktopButton } from '@wecode/features/vnc/VncDesktopButton'
+import { WorkspaceDesktopAction } from '@wecode/features/vnc/WorkspaceDesktopAction'
 import { openCloudDesktop } from '@wecode/features/vnc/openCloudDesktop'
 import { isInternalVncPageUrl } from '@wecode/features/vnc/session'
 
 export const cloudDesktopExtension: CloudDesktopExtension = {
   available: true,
   DeviceAction: VncDesktopButton,
+  WorkspaceAction: WorkspaceDesktopAction,
   isInternalPageUrl: isInternalVncPageUrl,
   open: openCloudDesktop,
 }
