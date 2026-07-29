9462de389070c8128db1fb06a8947ab73752606b
axb <qindi@staff.weibo.com>
2026-07-29 16:24:00 +0800

    fix(wework): use Codex transcript as message source (#2293)

--- executor/src/runtime_work/handler/queries.rs
+++ executor/src/runtime_work/handler/queries.rs
@@ -260,20 +260,7 @@ impl RuntimeWorkRpcHandler {
         } else {
             transcript_messages(&thread, &self.device_id)
         };
-        let mut messages = transcript_messages;
-        if local_link.as_ref().is_some_and(|link| link.running) {
-            append_missing_cached_user_messages(
-                &mut messages,
-                local_link.as_ref().map(cached_messages).unwrap_or_default(),
-            );
-        } else if local_link
-            .as_ref()
-            .is_some_and(|link| link.status.eq_ignore_ascii_case("failed"))
-        {
-            let cached = local_link.as_ref().map(cached_messages).unwrap_or_default();
-            append_missing_cached_user_messages(&mut messages, cached.clone());
-            append_missing_cached_failed_assistant_messages(&mut messages, cached);
-        }
+        let messages = transcript_messages;
         let running = local_execution_running;
         let message_count = messages.len();
         log_runtime_transcript_finished(RuntimeTranscriptLog {
