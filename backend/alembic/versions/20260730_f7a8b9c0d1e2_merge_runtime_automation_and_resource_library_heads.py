6443e9eb3c85657f6bbe6f5b05b0820ee89b8df1
yansheng3 <yansheng3@staff.weibo.com>
2026-07-30 16:22:49 +0800

    fix: Alembic merge revision

--- backend/alembic/versions/20260730_f7a8b9c0d1e2_merge_runtime_automation_and_resource_library_heads.py
+++ backend/alembic/versions/20260730_f7a8b9c0d1e2_merge_runtime_automation_and_resource_library_heads.py
@@ -2,10 +2,10 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-"""merge runtime automation and resource library heads
+"""merge knowledge artifacts and resource library heads
 
 Revision ID: f7a8b9c0d1e2
-Revises: d9e4f5a6b7c8, e6f7a8b9c013
+Revises: c8d2e3f4a5b6, e6f7a8b9c013
 Create Date: 2026-07-30
 
 """
@@ -14,7 +14,7 @@ from typing import Sequence, Union
 
 revision: str = "f7a8b9c0d1e2"
 down_revision: Union[str, Sequence[str], None] = (
-    "d9e4f5a6b7c8",
+    "c8d2e3f4a5b6",
     "e6f7a8b9c013",
 )
 branch_labels: Union[str, Sequence[str], None] = None
