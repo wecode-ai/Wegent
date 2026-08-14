c7e7f2666f46af4041f99dc5e544b86dedcf1453
axb <qindi@staff.weibo.com>
2026-08-14 19:32:27 +0800

    fix(wework): clear composer when resuming queue

--- wework/src/components/chat/ChatInput.tsx
+++ wework/src/components/chat/ChatInput.tsx
@@ -762,6 +762,7 @@ export const ChatInput = forwardRef<ChatInputHandle, ChatInputProps>(function Ch
       return
     }
     if (onResumeQueueWithInput) {
+      composerRef.current?.setValue('')
       onChange('')
       void onResumeQueueWithInput(valueOverride, options)
       return
