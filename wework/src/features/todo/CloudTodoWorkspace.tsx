f621ec99d6400af2c0d2a326a8db6e4c400d14a8
jiaqi62 <jiaqi62@staff.weibo.com>
2026-09-07 12:27:40 +0800

    fix(wework): restore incoming hook automation wiring

--- wework/src/features/todo/CloudTodoWorkspace.tsx
+++ wework/src/features/todo/CloudTodoWorkspace.tsx
@@ -4553,6 +4553,7 @@ export function CloudTodoWorkspace({
                   api={selectedProjectApi}
                   projectChatAgentApi={selectedProjectAgentApi}
                   projectAutomationApi={selectedProjectServices?.projectAutomationApi}
+                  projectIncomingHookApi={selectedProjectServices?.projectIncomingHookApi}
                   runtimeProfileApi={selectedProjectServices?.runtimeProfileApi}
                   executionApi={automationExecutionApi}
                   deviceApi={selectedProjectServices.deviceApi}
@@ -4589,7 +4590,6 @@ export function CloudTodoWorkspace({
                   api={selectedProjectApi}
                   aitableApi={aitableApi}
                   dwsApi={services.dwsApi}
-                  incomingHookApi={selectedProjectServices?.projectIncomingHookApi}
                   project={selectedProject}
                   boardCardDisplay={boardCardDisplay}
                   onProjectUpdated={updated => replaceProject(selectedProject, updated)}
